4709a8a385d7ad416023110d3ee79154f3b8a0f5
Shtvaliev <90838372+Shtvaliev@users.noreply.github.com>
2021-12-17 17:43:28 +0300

    Merge pull request #4
    * No doc
    
    * Doc 1.0
    
    * Doc 1.1

--- arcanoid.py
+++ arcanoid.py
@@ -1,18 +1,27 @@
 import pygame
 
 
-def make_blocks(Block, sprites):
-    blocks = [[Block() for j in range(10)] for i in range(17)]
+def make_blocks(block):
+    """Making blocks
+
+    Makes matrix of blocks and adds it to all and block sprites groups
+    :param block: block sprite
+    :return: objects of block sprite
+    """
+    blocks = [[block() for j in range(10)] for i in range(17)]
     for i in range(17):
         for j in range(10):
             blocks[i][j].rect.x = 42 * i
             blocks[i][j].rect.y = 40 * j
-            sprites['all_sprites'].add(blocks[i][j])
-            sprites['block_sprite'].add(blocks[i][j])
     return blocks
 
 
-def update_player(self, param):
+def update_player(self, default_parameters):
+    """Updating player's platform location
+
+    :param self: player sprite
+    :param default_parameters: parameters of game configuration
+    """
     self.speedx = 0
     keye = pygame.key.get_pressed()
     if keye[pygame.K_LEFT]:
@@ -20,61 +29,110 @@ def update_player(self, param):
     if keye[pygame.K_RIGHT]:
         self.speedx = 8
     self.rect.x += self.speedx
-    if self.rect.left > param['width']:
+    if self.rect.left > default_parameters['width']:
         self.rect.right = 0
     if self.rect.right < 0:
-        self.rect.left = param['width']
-    return self
+        self.rect.left = default_parameters['width']
 
 
-is_active = 0
-speed = 3
-x = 0
-y = 0
+def rebound_frames(i, j, x, y, speed, is_active, default_parameters):
+    """Rebound from frames rules
 
-
-def rebound_frames(i, j, x, y, speed, is_active, param):
-    if i > param['width'] - 2:
+    Changes direction of movement of the ball if it touches one of the frames and stop the game
+    if ball touched bottom frame
+    :param i: Ox ball coordinate
+    :param j: Oy ball coordinate
+    :param x: Ox axis speed of the ball
+    :param y: Oy axis speed of the ball
+    :param speed: default speed of ball
+    :param is_active: says if the game has to be continued
+    :param default_parameters: parameters of game configuration
+    :return: new Ox and Oy axis speed and answer if the game has to be continued
+    """
+    if i > default_parameters['width'] - 2:
         x = -speed
     if i < 2:
         x = speed
     if j < 2:
         y = speed
-    if j > param['height'] - 2:
+    if j > default_parameters['height'] - 2:
         is_active = 0
         x = 0
         y = 0
-    return x, y, is_active, speed
+    return x, y, is_active
 
 
 def activate_check(is_active, i_player, j_player, i_ball, j_ball):
+    """Activated check
+
+    Checks if the game has to be continued and return ball to the player's platform if not
+    :param is_active: says if the game has to be continued
+    :param i_player: Ox player's platform coordinate
+    :param j_player: Oy player's platform coordinate
+    :param i_ball: Ox ball coordinate
+    :param j_ball: Oy ball coordinate
+    :return: new Ox and Oy coordinates for the ball
+    """
     if is_active == 0:
         i_ball = i_player + 49
         j_ball = j_player - 10
     return i_ball, j_ball
 
 
-def activating(i_ball, j_ball, par1, par2, is_active, speed, x, y):
+def activating(par1, par2, is_active, speed, x, y):
+    """Game activating
+
+    Activate game or stop it after pressing buttons of start and stop
+    :param par1: is start button was pressed
+    :param par2: is stop button was pressed
+    :param is_active: says if the game has to be continued
+    :param speed: default speed of ball
+    :param x: Ox axis speed of the ball
+    :param y: Oy axis speed of the ball
+    :return: Ox and Oy axis speed of the ball and answer if the game has to be continued
+    """
     if par1 and is_active == 0:
         x = speed
         y = -speed
         is_active = 1
     if par2:
         is_active = 0
-        # i_ball = i_player + 49
-        # j_ball = j_player - 10
-    return x, y, is_active, i_ball, j_ball
+    return x, y, is_active
 
 
-def rebound_player(speed, local_param, y):
-    if local_param:
+def rebound_player(speed, local_parameter, y):
+    """Rebound from player's platform rules
+
+    Changes direction of movement of the ball if it touches the player's platform
+    :param speed: default speed of ball
+    :param local_parameter: is ball touched the platform
+    :param y: Oy axis speed of the ball
+    :return: new Oy axis speed of the ball
+    """
+    if local_parameter:
         y = -speed
     return y
 
 
 def rebound_block(ball_bottom, ball_left, ball_right, ball_top, block_top, block_right, block_left, block_bottom,
-                  local_param, x, y):
-    if local_param:
+                  local_parameter, x, y):
+    """Rebound from blocks rules
+
+    Changes direction of movement of the ball if it touches a block and delete the block
+    :param ball_bottom: coordinates of the bottom of the ball
+    :param ball_left: coordinates of the left edge of the ball
+    :param ball_right: coordinates of thr right edge of the ball
+    :param ball_top: coordinates of the top of the ball
+    :param block_top: coordinates of the top of the block
+    :param block_right: coordinates of the right edge of the block
+    :param block_left: coordinates of the left edge of the block
+    :param block_bottom: coordinates of the bottom of the bloock
+    :param local_parameter: is ball touched a block
+    :param x: Ox axis speed of the ball
+    :param y: Oy axis speed of the ball
+    :return: new Ox and Oy axis speed for the ball
+    """
+    if local_parameter:
         a = abs(ball_bottom - block_top) % 40
         b = abs(ball_left - block_right) % 42
         c = abs(ball_right - block_left) % 42
@@ -89,16 +147,24 @@ def rebound_block(ball_bottom, ball_left, ball_right, ball_top, block_top, block
     return x, y
 
 
-def update_ball(self, player, param, sprites, blocks):
+def update_ball(self, player, default_parameters, sprites, blocks):
+    """Updating ball location
+
+    Contains rules of ball movement
+    :param self: ball sprite
+    :param player: player sprite
+    :param default_parameters: parameters of game configuration
+    :param sprites: groups of sprites
+    :param blocks: list of blocks sprites
+    """
     global is_active
     global speed
     global x
     global y
     key = pygame.key.get_pressed()
 
-    x, y, is_active, self.rect.x, self.rect.y = activating(self.rect.x, self.rect.y,
-                                                           key[pygame.K_UP], key[pygame.K_DOWN], is_active, speed, x, y)
-    x, y, is_active, speed = rebound_frames(self.rect.x, self.rect.y, x, y, speed, is_active, param)
+    x, y, is_active= activating(key[pygame.K_UP], key[pygame.K_DOWN], is_active, speed, x, y)
+    x, y, is_active = rebound_frames(self.rect.x, self.rect.y, x, y, speed, is_active, default_parameters)
     self.rect.x, self.rect.y = activate_check(is_active, player.rect.x, player.rect.y, self.rect.x, self.rect.y)
     y = rebound_player(speed, pygame.sprite.spritecollideany(player, sprites['ball_sprite']), y)
     if self.rect.x // 42 < 17 and self.rect.y // 40 < 10:
@@ -113,17 +179,24 @@ def update_ball(self, player, param, sprites, blocks):
     self.rect.y += y
 
 
-def classes(param, sprites):
+def classes(default_parameters, sprites):
+    """Makes game objects
+
+    Makes sprites attending the game
+    :param default_parameters: parameters of game configuration
+    :param sprites: groups of sprites
+    :return: groups of sprites with added object in it
+    """
     class Player(pygame.sprite.Sprite):
         def __init__(self):
             pygame.sprite.Sprite.__init__(self)
             self.image = pygame.Surface((100, 10))
-            self.image.fill(param['green'])
+            self.image.fill(default_parameters['green'])
             self.rect = self.image.get_rect()
-            self.rect.center = (param['width'] / 2, param['height'] - 50)
+            self.rect.center = (default_parameters['width'] / 2, default_parameters['height'] - 50)
 
         def update(self):
-            update_player(self, param)
+            update_player(self, default_parameters)
     player = Player()
     sprites['all_sprites'].add(player)
 
@@ -131,10 +204,10 @@ def classes(param, sprites):
         def __init__(self):
             pygame.sprite.Sprite.__init__(self)
             self.image = pygame.Surface((30, 30))
-            self.image.fill(param['green'])
+            self.image.fill(default_parameters['green'])
             self.rect = self.image.get_rect()
-            self.rect.center = (param['width'] / 2, param['height'] / 2)
-    blocks = make_blocks(Block, sprites)
+            self.rect.center = (default_parameters['width'] / 2, default_parameters['height'] / 2)
+    blocks = make_blocks(Block)
     sprites['all_sprites'].add(blocks)
     sprites['block_sprite'].add(blocks)
 
@@ -142,12 +215,12 @@ def classes(param, sprites):
         def __init__(self):
             pygame.sprite.Sprite.__init__(self)
             self.image = pygame.Surface((5, 5))
-            self.image.fill(param['green'])
+            self.image.fill(default_parameters['green'])
             self.rect = self.image.get_rect()
-            self.rect.center = (param['width'] / 2, param['height'] / 2)
+            self.rect.center = (default_parameters['width'] / 2, default_parameters['height'] / 2)
 
         def update(self):
-            update_ball(self, player, param, sprites, blocks)
+            update_ball(self, player, default_parameters, sprites, blocks)
     ball = Ball()
     sprites['all_sprites'].add(ball)
     sprites['ball_sprite'].add(ball)
@@ -156,8 +229,17 @@ def classes(param, sprites):
 
 
 def main_parameters():
-    global param
-    param = {
+    """Set parameters
+
+    Contains parameters of game configuration
+    :return: parameters of game configuration
+    """
+    global default_parameters
+    global speed
+    global is_active
+    global x
+    global y
+    default_parameters = {
         'width': 700,
         'height': 700,
         'FPS': 60,
@@ -171,10 +253,19 @@ def main_parameters():
         'x': 0,
         'y': 0
     }
-    return param
+    speed = default_parameters['speed']
+    is_active = default_parameters['is_active']
+    x = default_parameters['x']
+    y = default_parameters['y']
+    return default_parameters
 
 
 def make_sprites():
+    """Sprites making
+
+    Makes all used groups of sprites
+    :return: groups of sprites
+    """
     sprites = {
         'all_sprites': pygame.sprite.Group(),
         'ball_sprite': pygame.sprite.Group(),
@@ -184,41 +275,30 @@ def make_sprites():
 
 
 def main():
-    param = main_parameters()
-    # all_sprites = make_sprites()
-    sprites = classes(param, make_sprites())
+    """Run function"""
+    default_parameters = main_parameters()
+    sprites = classes(default_parameters, make_sprites())
     
     pygame.init()
     pygame.mixer.init()
-    screen = pygame.display.set_mode((param['width'], param['height']))
+    screen = pygame.display.set_mode((default_parameters['width'], default_parameters['height']))
     pygame.display.set_caption("Acranoid")
     clock = pygame.time.Clock()
 
     running = True
     while running:
-        # Держим цикл на правильной скорости
-        clock.tick(param['FPS'])
-        # Ввод процесса (события)
+        clock.tick(default_parameters['FPS'])
         for event in pygame.event.get():
-            # check for closing window
             if event.type == pygame.QUIT:
                 running = False
 
-        # if pygame.sprite.spritecollideany(player, ball_sprite):
-        #     y = -2
-
-        # Обновление
         sprites['all_sprites'].update()
 
-        # Рендеринг
-        screen.fill(param['black'])
+        screen.fill(default_parameters['black'])
         sprites['all_sprites'].draw(screen)
-        # После отрисовки всего, переворачиваем экран
         pygame.display.flip()
 
     pygame.quit()
-
-    return 
 
 
 if __name__ == "__main__":
